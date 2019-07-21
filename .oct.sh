a40a5abcd50c51e005587da07b48b393232c2528
SaidGR <said5968480@gmail>
2019-07-20 21:49:52 -0600

    Scrypt Said

---
+++ .oct.sh
@@ -0,0 +1,14 @@
+command_not_found_handle() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+
+source $HOME/b-Said/.colores.sh
+
+.//b-Said/.inicio.sh
+
+
+PS1="$am $bgr Said$re$ve $ $re "
+
+
+
